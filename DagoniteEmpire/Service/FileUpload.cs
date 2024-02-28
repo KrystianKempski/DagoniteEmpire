5a8fcaabbc6de1b0bc49f0a1031e393eca50aba4
krystian kempski <krystian.kempski@gmail.com>
2024-02-29 00:07:20 +0100

    File input update

--- DagoniteEmpire/Service/FileUpload.cs
+++ DagoniteEmpire/Service/FileUpload.cs
@@ -1,16 +1,25 @@
 ﻿using DA_DataAccess;
+using DagoniteEmpire.Helper;
 using DagoniteEmpire.Service.IService;
+using ImageMagick;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Http;
+using Microsoft.JSInterop;
+
 
 namespace DagoniteEmpire.Service
 {
     public class FileUpload : IFileUpload
     {
         private readonly IWebHostEnvironment _environment;
+        private readonly IJSRuntime _jsRuntime;
+        private readonly IMagickNET _magickNet;
 
-        public FileUpload(IWebHostEnvironment environment)
+
+        public FileUpload(IWebHostEnvironment environment, IJSRuntime jsRuntime)
         {
             _environment = environment;
+            _jsRuntime = jsRuntime;
         }
         public bool DeleteFile(string filePath)
         {
@@ -24,20 +33,52 @@ namespace DagoniteEmpire.Service
 
         public async Task<string> UploadFile(IBrowserFile file)
         {
-            FileInfo fileInfo = new(file.Name);
-            var fileName = Guid.NewGuid().ToString() + fileInfo.Extension;
-            var folderDirectory = $"{_environment.WebRootPath}\\images\\portraits";
-            if (!Directory.Exists(folderDirectory))
+            try
             {
-                Directory.CreateDirectory(folderDirectory);
+                FileInfo fileInfo = new(file.Name);
+                var fileName = Guid.NewGuid().ToString() + ".webp";
+                var folderDirectory = $"{_environment.WebRootPath}\\images\\portraits";
+
+                if (!Directory.Exists(folderDirectory))
+                {
+                    Directory.CreateDirectory(folderDirectory);
+                }
+                var filePath = Path.Combine(folderDirectory, fileName);
+
+
+                await using (MemoryStream input = new MemoryStream())
+                {
+                    await file.OpenReadStream(5120000).CopyToAsync(input);
+                    using (var output = new MemoryStream())
+                    {
+                        input.Position = 0;
+                        using (var image = new MagickImage(input))
+                        {
+                            image.Format = MagickFormat.WebP;
+                            if (image.Width > 1024 || image.Height > 1024)
+                            {
+                                input.Position = input.Length;
+                                image.Resize(1024, 1024);
+                                image.Write(output);
+                            }
+                            else
+                            {
+                                input.Position = 0;
+                                await input.CopyToAsync(output);
+                            }
+                            await image.WriteAsync(filePath);
+                        }
+                    }
+                }
+
+                var fullPath = $"/images/portraits/{fileName}";
+                return fullPath;
             }
-            var filePath = Path.Combine(folderDirectory, fileName);
-
-            await using FileStream fs = new FileStream(filePath, FileMode.Create);
-            await file.OpenReadStream().CopyToAsync(fs);
-
-            var fullPath = $"/images/portraits/{fileName}";
-            return fullPath;
+            catch (Exception ex)
+            {
+                await _jsRuntime.ToastrError(ex.Message);
+            }
+            return "";
         }
     }
 }
