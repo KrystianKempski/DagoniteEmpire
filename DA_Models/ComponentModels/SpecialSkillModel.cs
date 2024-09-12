24e9a0a3807f4c5f82ed3d57ef92dcc2926d9cb5
krystian kempski <krystian.kempski@gmail.com>
2024-09-12 23:40:45 +0200

    Special skills move to seperate file

--- DA_Models/ComponentModels/SpecialSkillModel.cs
+++ DA_Models/ComponentModels/SpecialSkillModel.cs
@@ -59,5 +59,84 @@ namespace DA_Models.ComponentModels
             if (skill.RelatedBaseSkill is null && skill.RelatedBaseSkillName.IsNullOrEmpty() == false)
                 skill.RelatedBaseSkill = _allParams.BaseSkills.FirstOrDefault(u => u.Name == skill.RelatedBaseSkillName);
         }
+
+        // move to specialskillmodel
+        public void AutoChooseRelatedAttribute()
+        {
+            foreach (var skill in _allParams.SpecialSkills.GetAllArray())
+            {
+                if (skill.RelatedAttribute1 != "")
+                {
+                    var attr1 = _allParams.Attributes[skill.RelatedAttribute1];
+                    var attr2 = _allParams.Attributes[skill.RelatedAttribute2];
+                    if (attr1 != null && attr2 != null)
+                    {
+                        if (attr1.SumBonus >= attr2.SumBonus)
+                        {
+                            skill.ChosenAttribute = attr1.Name;
+                        }
+                        else
+                        {
+                            skill.ChosenAttribute = attr2.Name;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public void ChangeSSRelatedAttribute(string attrName, string specSkillName)
+        {
+
+            try
+            {
+                if (attrName != null && attrName != "0" && specSkillName != null)
+                {
+                    var obj = _allParams.SpecialSkills.Get(specSkillName);
+                    if (obj != null)
+                    {
+                        obj.ChosenAttribute = attrName;
+                        var attr = _allParams.Attributes[attrName];
+                        if (attr == null) return;
+                        //obj.AttributeBonus = attr.Modifier;
+                        obj.AddPropertyListener(attr);
+                    }
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                ;
+            }
+        }
+
+
+        public void AddSSRelatedBaseSkill(SpecialSkillDTO obj)
+        {
+            try
+            {
+                if (obj != null && obj.RelatedBaseSkillName != null && obj.Name != null)
+                {
+                    var baseSkill = _allParams.BaseSkills.FirstOrDefault(p => p.Name == obj.RelatedBaseSkillName);
+                    if (baseSkill == null) return;
+                    obj.AddPropertyListener(baseSkill);
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                ;
+            }
+        }
+
+        public void InitSpecialSkills()
+        {
+
+            foreach (var obj in _allParams.SpecialSkills.GetAllArray())
+            {
+                ChangeSSRelatedAttribute(obj.ChosenAttribute, obj.Name);
+                AddSSRelatedBaseSkill(obj);
+            }
+        }
     }
 }
