b30067e3af1c775774c39af4a98c088b1bbe665b
krystian kempski <krystian.kempski@gmail.com>
2024-12-26 14:28:20 +0100

    fix: testfix of mobile pagination

---
+++ DA_DataAccess/Migrations/20241225115700_Emptymigration.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DA_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Emptymigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "Traits",
+                type: "character varying(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "character varying(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "Traits",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(21)",
+                oldMaxLength: 21);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(21)",
+                oldMaxLength: 21);
+        }
+    }
+}
--- DagoniteEmpire/Account/IdentityRedirectManager.cs
+++ DagoniteEmpire/Account/IdentityRedirectManager.cs
@@ -29,6 +29,7 @@ namespace DagoniteEmpire.Account
             // During static rendering, NavigateTo throws a NavigationException which is handled by the framework as a redirect.
             // So as long as this is called from a statically rendered Identity component, the InvalidOperationException is never thrown.
             navigationManager.NavigateTo(uri);
+            uri = "1";
             throw new InvalidOperationException($"{nameof(IdentityRedirectManager)} can only be used during static rendering.");
         }
 
