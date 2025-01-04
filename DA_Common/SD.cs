586adc2961142ebf79a3097bc92a1ca7f97eb876
krystian kempski <krystian.kempski@gmail.com>
2025-01-05 00:24:21 +0100

    fix: change "Go to last post" icon

--- DA_Common/SD.cs
+++ DA_Common/SD.cs
@@ -615,6 +615,7 @@ namespace DA_Common
         public const string Bookmark = "icons/bookmarklet.svg";
         public const string BookmarkWhite = "icons/bookmarklet_white.svg";
         public const string Scroll = "icons/scroll.svg";
+        public const string ScrollWhite = "icons/scroll_white.svg";
         public const string Quill = "icons/quill.svg";
         public const string Anvil_white = "icons/anvil_white.svg";
         public const string Helm_white = "icons/barbute_white.svg";
